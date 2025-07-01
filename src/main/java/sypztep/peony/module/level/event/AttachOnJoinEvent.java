5fc1508fcfe8f0148a1071824e2a35feb179e6b5
chain <chainot@hotmail.co.th>
2025-07-02 01:45:24 +0700

    force sync a player stats when join

--- src/main/java/sypztep/peony/module/level/event/AttachOnJoinEvent.java
+++ src/main/java/sypztep/peony/module/level/event/AttachOnJoinEvent.java
@@ -1,5 +1,6 @@
 package sypztep.peony.module.level.event;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -12,11 +13,14 @@ public class AttachOnJoinEvent {
     public static void onEntityJoinWorld(EntityJoinLevelEvent event) {
         if (event.getEntity() instanceof LivingEntity livingEntity && !event.getLevel().isClientSide()) {
             livingEntity.getData(ModAttachments.LIVINGSTATS.get());
+            if (livingEntity instanceof Player player)
+                player.getData(ModAttachments.LIVINGSTATS.get()).syncToPlayer((ServerPlayer) player); // force sync a data on join
 
             if (!(event.getEntity() instanceof Player)) {
                 DamageTrackingAttachment tracking = livingEntity.getData(ModAttachments.DAMAGE_TRACKING.get());
                 tracking.setMaxHealth(livingEntity.getMaxHealth());
             }
+
         }
     }
 }
