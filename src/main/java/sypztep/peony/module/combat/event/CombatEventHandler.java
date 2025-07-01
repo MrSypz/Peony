17658e09eb73aa90448bd3cd6716c7cf408d1628
chain <chainot@hotmail.co.th>
2025-07-02 01:46:37 +0700

    //TODO: this part are monster apply special attack to player but for noreason it dupe event in Critical hit.. So fix in the future 2/7/2025

--- src/main/java/sypztep/peony/module/combat/event/CombatEventHandler.java
+++ src/main/java/sypztep/peony/module/combat/event/CombatEventHandler.java
@@ -1,16 +1,14 @@
 package sypztep.peony.module.combat.event;
 
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import sypztep.peony.module.combat.util.CombatHelper;
 import yesman.epicfight.api.neoforgeevent.playerpatch.DealDamageEvent;
-import yesman.epicfight.api.neoforgeevent.playerpatch.HurtEvent;
 import yesman.epicfight.api.utils.math.ValueModifier;
 
 public class CombatEventHandler {
-    @SubscribeEvent
+    @SubscribeEvent()
     public static void onDealDamage(DealDamageEvent.Pre event) {
         LivingEntity target = event.getTarget();
         LivingDamageEvent.Pre neoforgeEvent = event.getNeoForgeEvent();
@@ -25,14 +23,33 @@ public class CombatEventHandler {
         if (multiplier != 1.0F) {
             ValueModifier existing = event.getDamageSource().getDamageModifier();
             if (existing != null) existing.merge(ValueModifier.multiplier(multiplier));
-             else event.getDamageSource().setDamageModifier(ValueModifier.multiplier(multiplier));
+            else event.getDamageSource().setDamageModifier(ValueModifier.multiplier(multiplier));
         }
     }
-    @SubscribeEvent
-    public static void onPlayerHurt(HurtEvent.Post event) {
-        Player player = event.getPlayerPatch().getOriginal();
-        float originalDamage = event.getOriginalDamage();
-        float modifiedDamage = CombatHelper.modifyDamage(player, originalDamage, event.getDamageSource());
-        event.setDamage(modifiedDamage);
-    }
+    //TODO: this part are monster apply special attack to player but for noreason it dupe event in Critical hit.. So fix in the future 2/7/2025
+//    @SubscribeEvent
+//    public static void onDealDamage(DealDamageEvent.Pre event) {
+//        LivingEntity target = event.getTarget();
+//        LivingDamageEvent.Pre neoforgeEvent = event.getNeoForgeEvent();
+//
+//        float originalDamage = neoforgeEvent.getNewDamage();
+//
+//        float modifiedDamage = CombatHelper.modifyDamage(target, originalDamage, event.getDamageSource());
+//
+//        neoforgeEvent.setNewDamage(modifiedDamage);
+//
+//        float multiplier = modifiedDamage / originalDamage;
+//        if (multiplier != 1.0F) {
+//            ValueModifier existing = event.getDamageSource().getDamageModifier();
+//            if (existing != null) existing.merge(ValueModifier.multiplier(multiplier));
+//             else event.getDamageSource().setDamageModifier(ValueModifier.multiplier(multiplier));
+//        }
+//    }
+//    @SubscribeEvent
+//    public static void onPlayerHurt(HurtEvent.Post event) {
+//        Player player = event.getPlayerPatch().getOriginal();
+//        float originalDamage = event.getOriginalDamage();
+//        float modifiedDamage = CombatHelper.modifyDamage(player, originalDamage, event.getDamageSource());
+//        event.setDamage(modifiedDamage);
+//    }
 }
